180618f7145cfdaa2366f0a6f74d37dbc99603e0
Marencia <91967186+Marencia@users.noreply.github.com>
2021-12-04 14:24:37 -0300

    Descripcion de programa
    Descripción de lo que debería hacer

--- Comunicacion_bt_rf_arduino.ino
+++ Comunicacion_bt_rf_arduino.ino
@@ -59,3 +59,33 @@ void loop() {
     }
   } 
  } 
+
+
+/*
+Bueno acá comento como tendria que funcionar. El programa se separa en tres partes.
+PRIMERA:
+  Comunicación bluetooth: Envía a através de una aplicación del celular hecha con AppInventor,
+  un mensaje de ENTRANDO A LA HABITACIÓN (val='3') o SALIENDO DE LA HABITACIÓN (val='0').
+  Lo que debe hacer es que, cuando envío un '3', se entre al if val=='3' para ejecutar 
+  todas las instrucciones que hacen funcionar a los sensores y a la comunicacion de 
+  Radiofrecuencia. Si se envía el '0' debe salir del if val=='3'  y esperar un nuevo mensaje de '3' o nada.
+  
+SEGUNDA:
+  Hay dos sensores, uno sensa la entrada de un pasillo (sensor_IN) == HIGH) y el otro sensa 
+  la salida del mismo (sensor_OUT) == HIGH). En ambos casos se imprime en el monitor Serie
+  el mensaje que pertenece a cada acción. 
+  
+TERCERA:
+  Dentro de los if de los sensores, se configura la comunicación entre el emisor y receptor
+  de radiofrecuencia. Esto hace que se envie un mensaje de '1' si se está entrando o un '2'
+  si se está saliendo del pasillo.
+  
+Este es el funcionamiento general. El problema con este código es que cuando envio desde
+la app el '3' ingresa al if pero solamente habilita los sensores una sola vez. Después
+debe recibir si o si otro mensaje de '3' para verificar los if de sensor. O directamente
+se cuelga y no hace más nada.
+Con el while y la variable gancho que están comentados logro que los sensores esten siempre habilitados, osea 
+que se quede dentro del if de val='3', pero solo sale de el cuando sensor_OUT está
+en HIGH, y no cuando desde la app le envío '0'. 
+La otra opción que pensaba era utilizar un case, pero es casi lo mismo que los if.
+*/
